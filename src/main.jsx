9e64280b1fbe4ada64fcfc1f2e5285f8e046d1c7
Moody Omar <moody@Moodys-MacBook-Pro.local>
2025-06-17 18:30:56 +0300

    init pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import App from './App';
+import LoginPage from './pages/LoginPage';
+import MealsPage from './pages/MealsPage';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/meals" element={<MealsPage />} />
+    </Routes>
+  </BrowserRouter>
+);
